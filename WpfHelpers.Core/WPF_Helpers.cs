7593e43c6f56fc570da7d4015bfa804572a10c3d
michael@roop.ca <michael@roop.ca>
2020-04-09 22:49:06 -0300

    Initial check-in

---
+++ WpfHelpers.Core/WPF_Helpers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Controls;
+
+namespace WpfHelperClasses.Core {
+
+    public static class WPF_ControlHelpers {
+
+        public static void ResizeToWidest(params Button[] buttons) {
+            double maxWidth = 0;
+            foreach (Button button in buttons) {
+                maxWidth = Math.Max(maxWidth, button.ActualWidth);
+            }
+
+            foreach (Button button in buttons) {
+                button.Width = maxWidth;
+            }
+        }
+
+    }
+}
