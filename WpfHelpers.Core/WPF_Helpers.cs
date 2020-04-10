9d1889ba8a28999cb6dafb4a19fa565272297d40
michael@roop.ca <michael@roop.ca>
2020-04-10 16:35:43 -0300

    Sett min and max to a button array to allow resize later on. Called before button changes

--- WpfHelpers.Core/WPF_Helpers.cs
+++ WpfHelpers.Core/WPF_Helpers.cs
@@ -5,6 +5,29 @@ namespace WpfHelperClasses.Core {
 
     public static class WPF_ControlHelpers {
 
+        /// <summary>Call at class startup before buttons are rendered and before content change</summary>
+        /// <param name="buttons"></param>
+        public static void ForceButtonMinMax(params Button[] buttons) {
+            foreach (Button button in buttons) {
+                button.MinWidth = 10;
+                button.MaxWidth = 1000;
+                button.Width = Double.NaN;
+            }
+        }
+
+
+        /// <summary>Set the button array to width of widest button</summary>
+        /// <remarks>
+        /// To make this work call when all of the .SizeChanged of all the
+        /// buttons have fired. 
+        /// At the point that all have fired you need to disconnect the
+        /// event to avoid the 1% added below to refire in an endless loop
+        /// 
+        /// Before content changes call the ForceButtonsMinMax so that this
+        /// method will be able to properly resize buttons both ways
+        /// 
+        /// </remarks>
+        /// <param name="buttons"></param>
         public static void ResizeToWidest(params Button[] buttons) {
             double maxWidth = 0;
             foreach (Button button in buttons) {
@@ -12,7 +35,13 @@ namespace WpfHelperClasses.Core {
             }
 
             foreach (Button button in buttons) {
-                button.Width = maxWidth;
+                // If you set Width to other than NaN it turns off auto sizing
+                // https://docs.microsoft.com/en-us/dotnet/api/system.windows.frameworkelement.width?view=netframework-4.8
+                //button.Width = maxWidth;
+                // The tiny % increase forces another redraw so you get the right size
+                maxWidth += 1.0001;
+                button.MaxWidth = maxWidth  + 1;
+                button.MinWidth = maxWidth - 1;
             }
         }
 
